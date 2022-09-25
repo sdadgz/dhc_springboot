89ac7c929912c9cb13b992c78706996a517e16e5
sdadgz <pengchengma5@gmail.com>
2022-09-25 16:52:50 +0800

    init

---
+++ src/main/java/cn/sdadgz/dhc_springboot/DhcSpringbootApplication.java
@@ -0,0 +1,13 @@
+package cn.sdadgz.dhc_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhcSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DhcSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/sdadgz/dhc_springboot/DhcSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.sdadgz.dhc_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhcSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
