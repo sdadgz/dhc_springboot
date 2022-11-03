0b72f3dbfbfbb34b9186dbfa838af9b3326692d4
sdadgz <pengchengma5@gmail.com>
2022-11-03 15:18:34 +0800

    3

--- src/main/java/cn/sdadgz/dhc_springboot/config/JwtInterceptorConfig.java
+++ src/main/java/cn/sdadgz/dhc_springboot/config/JwtInterceptorConfig.java
@@ -1,7 +1,6 @@
 package cn.sdadgz.dhc_springboot.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
