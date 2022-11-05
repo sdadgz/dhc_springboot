c7ae2749e8d55d985fba969f708b07c07b0acafe
sdadgz <pengchengma5@gmail.com>
2022-11-05 14:09:56 +0800

    5

--- src/main/java/cn/sdadgz/dhc_springboot/controller/CarouselController.java
+++ src/main/java/cn/sdadgz/dhc_springboot/controller/CarouselController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 
 /**
